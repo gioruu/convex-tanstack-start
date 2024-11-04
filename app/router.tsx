39efe8476e2a53da87dbcdc364647bf63abb3d9c
Tom Ballinger <tom@convex.dev>
2024-11-04 14:10:05 -0800

    simplify

--- app/router.tsx
+++ app/router.tsx
@@ -1,21 +1,7 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-import { QueryClient } from '@tanstack/react-query'
-import { routerWithQueryClient } from '@tanstack/react-router-with-query'
 
 export function createRouter() {
-  /*
-  const queryClient: QueryClient = new QueryClient()
-
-  const router = routerWithQueryClient(
-    createTanStackRouter({
-      routeTree,
-      defaultPreload: 'intent',
-      context: { queryClient },
-    }),
-    queryClient,
-  )
-  */
   const router = createTanStackRouter({
     routeTree,
   })
